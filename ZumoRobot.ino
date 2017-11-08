500a34f75b2d839499ad9c08e1f19a5c4b8cd8c6
André Storhaug <andre.storhaug@mimer.no>
2017-11-08 19:09:37 +0100

    Updated dependencies and .ino file

--- ZumoRobot.ino
+++ ZumoRobot.ino
@@ -1,23 +1,41 @@
-/*
+/**
  * Name:      ZumoRobot.ino
  * Created:   08/10/2017  13:00:00 PM
- * Authors:   André Storaug, Eirik Thue, ....
+ * Authors:   André Storaug, Christian Honningdal Leira,
+ *              Tormod Lysvold Sjømæling, Eirik Thue,
+ *              Vebjørn Tomren og Sander Joachim Skarmyr.
  */
- #include "ZumoMotors.h"
 
+/* Libraries */
+#include <ZumoBuzzer.h>
+#include <Pushbutton.h>
+#include <ZumoMotors.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
 
+/* Global constants */
 const int LED_PIN = 13;
 
+/* Global variables */
+int var;
+
+/**
+ * Main setup function. Only runs ones.
+ */
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
 }
 
+/**
+ * Main loop.
+ * Responsible for main program functionallity.
+ */
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_PIN, HIGH);
 
-/*
+
   for (int time = 0; time <= 500; time++) {
     int speed = 100;
 
@@ -33,10 +51,4 @@ void loop() {
     motors.setRightSpeed(speed+50);
     delay(2);
   }
-*/
-
-
-
-
-
 }
